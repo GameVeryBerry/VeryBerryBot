05b793355785f954941b5443554caa2333a2c9f0
YdeaGames <ydeagames@gmail.com>
2019-12-02 02:09:16 +0900

    クリーンアップ

--- spreadsheettest1.py
+++ spreadsheettest1.py
@@ -1,12 +1,8 @@
-from __future__ import print_function
 import pickle
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-import gspread
-import os
-import pickle
 # pip install pygsheets
 import pygsheets
 
@@ -14,13 +10,10 @@ import pygsheets
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1oFyoE5V38GeSM65bViTJCPgQYVnwwlmv2-bwCHhFgr0'
-SAMPLE_RANGE_NAME = 'Attendance!A2:E'
+SPREADSHEET_ID = '1oFyoE5V38GeSM65bViTJCPgQYVnwwlmv2-bwCHhFgr0'
 
-def get_index(l, x, default=False):
-    return l.index(x) if x in l else default
 
-def main():
+def get_credentials():
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
     """
@@ -42,65 +35,23 @@ def main():
         # Save the credentials for the next run
         with open('credentials/token.pickle', 'wb') as token:
             pickle.dump(creds, token)
+    return creds
 
+
+def get_index(l, x, default=False):
+    return l.index(x) if x in l else default
+
+
+def main():
+    creds = get_credentials()
     gc = pygsheets.authorize(custom_credentials=creds)
-    wks = gc.open_by_key(SAMPLE_SPREADSHEET_ID).sheet1
+    wks = gc.open_by_key(SPREADSHEET_ID).sheet1
     #Access all of the record inside that
     ids = wks.get_col(1)
     row = get_index(ids, '9', None)
     if not row == None:
         data = wks.get_row(row + 1)
         print(data)
-    # credentials = {
-    #     'Credentials': creds.credentials
-    # }
-    # gc = gspread.authorize(credentials)
-
-    # # Open a worksheet from spreadsheet with one shot
-    # wks = gc.open_by_key(SAMPLE_SPREADSHEET_ID).sheet1
-
-    # wks.update_acell('B2', "it's down there somewhere, let me take another look.")
-
-    # cell_list = wks.range('A1:B7')
-
-    # service = build('sheets', 'v4', credentials=creds)
-
-    # # Call the Sheets API
-    # sheet = service.spreadsheets()
-    # # result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-    # #                             range=SAMPLE_RANGE_NAME).execute()
-    # # values = result.get('values', [])
-
-    # values = [
-    #     [
-    #         # Cell values ...
-    #         "AAA",
-    #         "BBB",
-    #     ],
-    #     # Additional rows
-    # ]
-    # data = [
-    #     {
-    #         'range': 'Attendance!A2:E',
-    #         'values': values
-    #     },
-    #     # Additional ranges to update ...
-    # ]
-    # body = {
-    #     'valueInputOption': 'RAW',
-    #     'data': data
-    # }
-    # result = sheet.values().batchUpdate(
-    #     spreadsheetId=SAMPLE_SPREADSHEET_ID, body=body).execute()
-    # print('{0} cells updated.'.format(result.get('totalUpdatedCells')))
-
-    # if not values:
-    #     print('No data found.')
-    # else:
-    #     print('Name, Major:')
-    #     for row in values:
-    #         # Print columns A and E, which correspond to indices 0 and 4.
-    #         print('%s, %s' % (row[0], row[1]))
 
 if __name__ == '__main__':
     main()
